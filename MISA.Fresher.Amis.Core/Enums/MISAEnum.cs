1d335a8b45dd0cd6219027ded23b041cf72eafc7
Your Name <you@example.com>
2021-12-30 16:28:28 +0700

    commit all 30/12/2021

---
+++ MISA.Fresher.Amis.Core/Enums/MISAEnum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MISA.Fresher.Amis.Core.Enums
+{
+    public enum Gender
+    {
+        /// <summary>
+        /// Nữ
+        /// </summary>
+        Male = 1,
+        /// <summary>
+        /// Nam
+        /// </summary>
+        Female = 0,
+        /// <summary>
+        /// Khác
+        /// </summary>
+        Other = 2
+    }
+    public enum Status
+    {
+        /// <summary>
+        /// Lỗi validate không hợp lệ
+        /// </summary>
+        BadRequestError = 400,
+        /// <summary>
+        /// Lỗi server
+        /// </summary>
+        ServerError = 500
+    }
+}
+
